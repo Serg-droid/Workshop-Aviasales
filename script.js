d3af6623e01a6d2afa8652dfa4742814d8c5bd8b
Serg-droid <61561342+Serg-droid@users.noreply.github.com>
2020-03-19 02:22:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,9 +5,35 @@ const formSearch = document.querySelector('.form-search'),
 	dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
 	inputDateDepart = document.querySelector('.input__date-depart');
 
-// Список городов
-const city = ['Москва', 'Санкт-Петербург', 'Самара', 'Минск', 'Караганда', 
-	'Керчь', 'Одесса', 'Ухань', 'Екатеринбург', 'Волгоград'];
+// Данные --------------------------
+let city = [];
+
+
+const citiesAPI = 'http://api.travelpayouts.com/data/ru/cities.json',
+	PROXY = 'https://cors-anywhere.herokuapp.com/',
+	API_KEY = '51448a9e4946b299543e01e858e8b616',
+	calendar = 'http://min-prices.aviasales.ru/calendar_preload';
+
+
+//// Функции -------------------------
+
+const getData = (url, callback) => {
+	const request = new XMLHttpRequest();
+
+	request.open('GET', url);
+
+	request.addEventListener('readystatechange', () => {
+		if (request.readyState !== 4) return;
+
+		if (request.status === 200) {
+			callback(request.response);
+		} else {
+			console.error(request.status);
+		}
+	});
+
+	request.send();
+};
 
 // Функция вывода списка
 const showCity = (dropdown, input) => {
@@ -21,14 +47,14 @@ const showCity = (dropdown, input) => {
 			}
 
 			const filterCity = city.filter((item) => {
-				const fixItem = item.toLowerCase();
+				const fixItem = item.name.toLowerCase();
 				return fixItem.includes(input.value.toLowerCase());
 			});
 			
 			filterCity.forEach((item) => {
 				const li = document.createElement('li');
 				li.classList.add('dropdown__city');
-				li.textContent = item;
+				li.textContent = item.name;
 				dropdown.append(li);
 			});
 
@@ -47,6 +73,10 @@ const autoFill = (event, dropdown, input) => {
 };
 
 
+
+
+
+
 // Вывод списка предлагаемых результатов ввода. Куда
 inputCitiesFrom.addEventListener('input', showCity(dropdownCitiesFrom, inputCitiesFrom));
 
@@ -58,5 +88,28 @@ dropdownCitiesFrom.addEventListener('click', () => autoFill(event, dropdownCitie
 
 // Автозаполнение формы. Откуда
 dropdownCitiesTo.addEventListener('click', () => autoFill(event, dropdownCitiesTo, inputCitiesTo));
+
+
+////// Вызовы функций -----------------------
+
+
+// Получаем и фильтруем города
+getData(PROXY + citiesAPI, (data) => {
+	const dataCitites = JSON.parse(data);
+
+	city = dataCitites.filter((item) => {
+		if (item.name) {return true}
+		return false;
+	});
+});
+
+// получаем цену на 25 мая, рейс: Екатеринбург - Калининград
+// Выкидывает ошибку 400. При переходе по ссылке в API, никакой базы данных там не наблюдаю
+getData(PROXY + calendar, (data) => {
+	console.log(data)
+});
+
+
+
 
 
